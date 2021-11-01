4ec00822dbe06e9546a5bfa25b578d3e32d278a2
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2021-11-01 20:20:57 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,17 @@
+let _1 = 0
+let _2 = 0
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("your_ssid", "your_pwd")
+basic.forever(function () {
+    _2 += 1
+    basic.pause(1000)
+})
+basic.forever(function () {
+    ESP8266_IoT.connectThingSpeak()
+    ESP8266_IoT.setData(
+    "PME0W3E16ZG9XBZE",
+    _1,
+    _2
+    )
+    ESP8266_IoT.uploadData()
+})
